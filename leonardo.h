830849ea33644b051525a5b1443ba3dcb6ffde7c
Alec Fenichel <fenichelar@gatech.edu>
2014-12-01 13:06:41 +0100

    Finish header files for Uno and Leonardo

--- leonardo.h
+++ leonardo.h
@@ -7,13 +7,31 @@
 #ifndef Leonardo_H
 #define Leonardo_H
 
-#define ANALOGOFFSET 0
+#define ANALOGOFFSET 14
 
 struct pinMapping pinMappings[] = {
 // Digital
-{&PINA,&PORTA,&DDRA,1<<0},
+{&PIND,&PORTD,&DDRD,1<<2},
+{&PIND,&PORTD,&DDRD,1<<3},
+{&PIND,&PORTD,&DDRD,1<<1},
+{&PIND,&PORTD,&DDRD,1<<0},
+{&PIND,&PORTD,&DDRD,1<<4},
+{&PINC,&PORTC,&DDRC,1<<6},
+{&PIND,&PORTD,&DDRD,1<<7},
+{&PINE,&PORTE,&DDRE,1<<6},
+{&PINB,&PORTB,&DDRB,1<<4},
+{&PINB,&PORTB,&DDRB,1<<5},
+{&PINB,&PORTB,&DDRB,1<<6},
+{&PINB,&PORTB,&DDRB,1<<7},
+{&PIND,&PORTD,&DDRD,1<<6},
+{&PINC,&PORTC,&DDRC,1<<7},
 // Analog
-{&PINA,&PORTA,&DDRA,1<<0}
+{&PINF,&PORTF,&DDRF,1<<7},
+{&PINF,&PORTF,&DDRF,1<<6},
+{&PINF,&PORTF,&DDRF,1<<5},
+{&PINF,&PORTF,&DDRF,1<<4},
+{&PINF,&PORTF,&DDRF,1<<1},
+{&PINF,&PORTF,&DDRF,1<<0},
 };
 
 #endif
--- uno.h
+++ uno.h
@@ -7,13 +7,31 @@
 #ifndef Uno_H
 #define Uno_H
 
-#define ANALOGOFFSET 0
+#define ANALOGOFFSET 14
 
 struct pinMapping pinMappings[] = {
 // Digital
-{&PINA,&PORTA,&DDRA,1<<0},
+{&PIND,&PORTD,&DDRD,1<<0},
+{&PIND,&PORTD,&DDRD,1<<1},
+{&PIND,&PORTD,&DDRD,1<<2},
+{&PIND,&PORTD,&DDRD,1<<3},
+{&PIND,&PORTD,&DDRD,1<<4},
+{&PIND,&PORTD,&DDRD,1<<5},
+{&PIND,&PORTD,&DDRD,1<<6},
+{&PIND,&PORTD,&DDRD,1<<7},
+{&PINB,&PORTB,&DDRB,1<<0},
+{&PINB,&PORTB,&DDRB,1<<1},
+{&PINB,&PORTB,&DDRB,1<<2},
+{&PINB,&PORTB,&DDRB,1<<3},
+{&PINB,&PORTB,&DDRB,1<<4},
+{&PINB,&PORTB,&DDRB,1<<5},
 // Analog
-{&PINA,&PORTA,&DDRA,1<<0}
+{&PINC,&PORTC,&DDRC,1<<0},
+{&PINC,&PORTC,&DDRC,1<<1},
+{&PINC,&PORTC,&DDRC,1<<2},
+{&PINC,&PORTC,&DDRC,1<<3},
+{&PINC,&PORTC,&DDRC,1<<4},
+{&PINC,&PORTC,&DDRC,1<<5}
 };
 
 #endif
