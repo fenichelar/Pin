1644389f4b738dcd62a55bad0944c9a8ae2fa0ce
Alec Fenichel <fenichelar@gatech.edu>
2015-07-11 13:57:49 -0400

    Add for simultaneous operations on multiple pins

---
+++ examples/Pin-Group/Pin-Group.ino
@@ -0,0 +1,33 @@
+/**
+  @file Pin-Group.ino
+  @author Alec Fenichel
+  @brief Pin group example
+  @details For advanced users only: simultaneously switch an array of multiple Pins between outputs and inputs
+*/
+
+#include <Pin.h>  // Include Pin library
+#include <PinGroup.h>  // Include Pin library group functions
+
+// The Pins used in this array must all be on the same DDR and PORT registers
+// Look at the coresponding file in the boards directory to determine what register each pin is on
+Pin myPins[] = {0,1,2};  // Create array of Pin objects for digital pins labelled 0-2 on any of the supported boards
+
+/**
+  Called at start
+ */
+void setup() {
+}
+
+/**
+  Called continously after setup
+ */
+void loop() {
+  setOutputLow(myPins);  // Simultaneously set array of Pins to output low
+  
+  delay(2);  // Wait 2 milliseconds
+  
+  setInput(myPins);  // Simultaneously set array of Pins to input mode
+  
+  delay(2);  // Wait 2 milliseconds
+
+}
---
+++ src/PinGroup.h
@@ -0,0 +1,157 @@
+/**
+	@file PinGroup.h
+	@author Alec Fenichel
+	@brief Arduino Pin library group functions
+	@details Arduino library for fast access to Arduino I/O pins using port manipulation with functions for simultaneous operations on multiple Pins
+ */
+
+
+#include "Pin.h"
+
+
+#ifndef PinGroup_H
+#define PinGroup_H
+
+
+// ################################# Defines #################################
+
+#define DDRS_HIGH (*pins[0].getDDR() |= offset)  ///< Set the DDR register to HIGH for an array of pins
+#define DDRS_LOW (*pins[0].getDDR() &= ~offset)  ///< Set the DDR register to LOW for an array of pins
+
+#define PORTS_HIGH (*pins[0].getPORT() |= offset)  ///< Set the PORT register to HIGH for an array of pins
+#define PORTS_LOW (*pins[0].getPORT() &= ~offset)  ///< Set the PORT register to LOW for an array of pins
+
+#define MERGE_OFFSET(PINS,LEN) { for (int i = 0; i < (LEN); i++) { offset |= (PINS)[i].getOffset();} }  ///< Merge the offsets of multiple pins in an array
+
+
+// ################################# Non-member Methods #################################
+
+// #################### Input ####################
+
+/**
+	Set the pin mode of multiple pins to input
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+void setInput(Pin (&pins)[N]) {
+	uint8_t offset = 0;
+	MERGE_OFFSET(pins,N)
+	DDRS_LOW;
+}
+
+/**
+	Set the pin pullup resistor of multiple pins to on
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+void setPullupOn(Pin (&pins)[N]) {
+	uint8_t offset = 0;
+	MERGE_OFFSET(pins,N)
+	PORTS_HIGH;
+}
+
+/**
+	Set the pin pullup resistor of multiple pins to off
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+void setPullupOff(Pin (&pins)[N]) {
+	uint8_t offset = 0;
+	MERGE_OFFSET(pins,N)
+	PORTS_LOW;
+}
+
+/**
+	Set the pin mode of multiple pins to input and the pin pullup resistor to on
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+void setInputPullupOn(Pin (&pins)[N]) {
+	uint8_t offset = 0;
+	MERGE_OFFSET(pins,N)
+	DDRS_LOW;
+	PORTS_HIGH;
+}
+
+/**
+	Set the pin mode of multiple pins to input and the pin pullup resistor to off
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+void setInputPullupOff(Pin (&pins)[N]) {
+	uint8_t offset = 0;
+	MERGE_OFFSET(pins,N)
+	DDRS_LOW;
+	PORTS_LOW;
+}
+
+// #################### Output ####################
+
+/**
+	Set the pin mode of multiple pins to output
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+ void setOutput(Pin (&pins)[N]) {
+ 	uint8_t offset = 0;
+ 	MERGE_OFFSET(pins,N)
+ 	DDRS_HIGH;
+ }
+
+/**
+	Set the pin output of multiple pins to HIGH
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+void setHigh(Pin (&pins)[N]) {
+	uint8_t offset = 0;
+	MERGE_OFFSET(pins,N)
+	PORTS_HIGH;
+}
+
+/**
+	Set the pin output of multiple pins to LOW
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+void setLow(Pin (&pins)[N]) {
+	uint8_t offset = 0;
+	MERGE_OFFSET(pins,N)
+	PORTS_LOW;
+}
+
+/**
+	Set the pin mode of multiple pins to output and the pin output to HIGH
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+void setOutputHigh(Pin (&pins)[N]) {
+	uint8_t offset = 0;
+	MERGE_OFFSET(pins,N)
+	DDRS_HIGH;
+	PORTS_HIGH;
+}
+
+/**
+	Set the pin mode of multiple pins to output and the pin output to LOW
+
+	@param pins an array of pins that use the same registers
+ */
+template<size_t N>
+void setOutputLow(Pin (&pins)[N]) {
+	uint8_t offset = 0;
+	MERGE_OFFSET(pins,N)
+	DDRS_HIGH;
+	PORTS_LOW;
+}
+
+#endif
